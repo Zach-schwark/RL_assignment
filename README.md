83343c11cf804b492edf4c0f6f895fcd485653a8
Zach Schwark <zachs1708@gmail.com>
2024-10-07 15:51:52 +0200

    add start of ppo and stable baselines links

--- README.md
+++ README.md
@@ -3,9 +3,13 @@ Power Grid Assignment for the Reinforcement Learning course for Wits Computer Sc
 
 
 
+Stable Baselines 3 uses PyTorch rather than Stable Baselines 1 that uses Tensorflow 
+
 ## Useful Links:
 
 - [Github for Grid2OP](https://github.com/rte-france/Grid2Op)
 - [Documentation for Grid2Op](https://grid2op.readthedocs.io/en/latest/)
 - [Grid2Op observation space](https://grid2op.readthedocs.io/en/latest/observation.html)
 - [Grid2Op action space](https://grid2op.readthedocs.io/en/latest/action.html)
+- [Stable Baselines 3 Github](https://github.com/DLR-RM/stable-baselines3)
+- [Stable Baselines 3 Docs](https://stable-baselines3.readthedocs.io/en/master/)
---
+++ ppo/ppo.py
@@ -0,0 +1,66 @@
+import gymnasium as gym
+import grid2op
+from provided_wrapper.env import Gym2OpEnv
+
+
+def main():
+    # Random agent interacting in environment #
+
+    max_steps = 3
+
+    env = Gym2OpEnv()
+
+    print("#####################")
+    print("# OBSERVATION SPACE #")
+    print("#####################")
+    print(env.observation_space)
+    print("#####################\n")
+
+    print("#####################")
+    print("#   ACTION SPACE    #")
+    print("#####################")
+    print(env.action_space)
+    print("#####################\n\n")
+
+    curr_step = 0
+    curr_return = 0
+
+    is_done = False
+    obs, info = env.reset()
+    print(f"step = {curr_step} (reset):")
+    print(f"\t obs = {obs}")
+    print(f"\t info = {info}\n\n")
+
+    while not is_done and curr_step < max_steps:
+        action = env.action_space.sample()
+        obs, reward, terminated, truncated, info = env.step(action)
+
+        curr_step += 1
+        curr_return += reward
+        is_done = terminated or truncated
+
+        print(f"step = {curr_step}: ")
+        print(f"\t obs = {obs}")
+        print(f"\t reward = {reward}")
+        print(f"\t terminated = {terminated}")
+        print(f"\t truncated = {truncated}")
+        print(f"\t info = {info}")
+
+        # Some actions are invalid (see: https://grid2op.readthedocs.io/en/latest/action.html#illegal-vs-ambiguous)
+        # Invalid actions are replaced with 'do nothing' action
+        is_action_valid = not (info["is_illegal"] or info["is_ambiguous"])
+        print(f"\t is action valid = {is_action_valid}")
+        if not is_action_valid:
+            print(f"\t\t reason = {info['exception']}")
+        print("\n")
+
+    print("###########")
+    print("# SUMMARY #")
+    print("###########")
+    print(f"return = {curr_return}")
+    print(f"total steps = {curr_step}")
+    print("###########")
+
+
+if __name__ == "__main__":
+    main()
