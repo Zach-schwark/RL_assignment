c7d47807f7a79161411af51138a9ac5702560164
Zach Schwark <zachs1708@gmail.com>
2024-10-21 20:21:08 +0200

    add code for iterationns

--- ppo/PPO.md
+++ ppo/PPO.md
@@ -4,3 +4,9 @@ Mark down file for explaination, tracking probelms, making notes etc
 
 This file will also be used to track the differnet iterations of the algorithm/model.
 ie what changes we made and what iterations and what we noticed, how it improved and why we did the changes we did.
+
+
+
+
+having a load alone on a busbar (=without having one powerline connected to the same busbar) is equivalent to disconnect it, and will lead to a game over.
+having a generator alone on a busbar (=without having one powerline connected to the same busbar) is also equivalent to disconnect it, it will also lead to a game over.
--- ppo/recurrent_ppo.py
+++ ppo/recurrent_ppo.py
@@ -7,31 +7,15 @@ display_tqdm = False  # this is set to False for ease with the unitt test, feel 
 import wandb
 sys.path.insert(0,"./")
 from provided_wrapper.env import Gym2OpEnv
-from stable_baselines3 import PPO
+from sb3_contrib import RecurrentPPO
 from wandb.integration.sb3 import WandbCallback
 import torch
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 
 def main():
-    
-    if len(sys.argv) != 2:
-        exit()
-    
-    version = sys.argv[1]
-    
-    baseline= False
-    first = False
-    second = False
-    
-    if version == 'baseline':
-        baseline = True
-    elif version == 'first':
-        first = True
-    elif version == 'second':
-        second = True
-        
-    env = Gym2OpEnv(baseline=baseline, first_iteraion=first,second_iteraion=second)
+
+    env = Gym2OpEnv(baseline=False, first_iteraion=False,second_iteraion=True)
     env = Monitor(env)
     
     print("#####################")
@@ -43,37 +27,18 @@ def main():
 
     run = wandb.init(
         project="RL_project",
-        name = "PPO_FirstItr_BoxAct_attrObs",
+        name = "Recurrent_PPO",
         sync_tensorboard=True,  # auto-upload sb3's tensorboard metrics
         monitor_gym=True,  # auto-upload the videos of agents playing the game
         save_code=False,  # optional
     )
 
 
-    model = PPO("MultiInputPolicy",
+    model = RecurrentPPO("MultiInputLstmPolicy",
                 env,
                 verbose=1,
                 device=device,
-                tensorboard_log=f"runs/{run.id}",
-                learning_rate=0.0003,
-                n_steps=2048,
-                batch_size=64,
-                n_epochs=10,
-                gamma=0.99,
-                gae_lambda=0.95,
-                clip_range=0.2,
-                clip_range_vf=None,
-                normalize_advantage=True,
-                ent_coef=0.0,
-                vf_coef=0.5,
-                max_grad_norm=0.5,
-                use_sde=False,
-                sde_sample_freq=-1,
-                rollout_buffer_class=None,
-                rollout_buffer_kwargs=None,
-                target_kl=None,
-                stats_window_size=100,
-                _init_setup_model=True)
+                tensorboard_log=f"runs/{run.id}")
     
     
     model.learn(total_timesteps=25000, callback=WandbCallback(gradient_save_freq=100,model_save_path=f"models/{run.id}",verbose=2), progress_bar=True)
